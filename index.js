f461392026160d3d49eea289bcd0094626b34882
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-02-15 17:02:31 +0200

    шаг 4

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
+// удобный парс аргументов командной строки
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import contacts from './contacts.js';
+
+const argv = yargs(hideBin(process.argv)).argv;
 const { listContacts, getContactById, removeContact, addContact } = contacts;
 
-// Получаем весь список контакстов в виде таблицы
-listContacts();
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            listContacts();
+            break;
+
+        case 'get':
+            getContactById(id);
+            break;
+
+        case 'add':
+            addContact(name, email, phone);
+            break;
+
+        case 'remove':
+            removeContact(id);
+            break;
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+invokeAction(argv);
+
+// Получаем весь список контактов в виде таблицы
+// listContacts();
 
 // Получаем контакт по id
-getContactById(3);
+// getContactById(3);
 
 // Добавляем контакт (name, email, phone)
-addContact('David', 'david.alyaska@mail.com', '(562) 569-8823');
+// addContact('David', 'david.alyaska@mail.com', '(562) 569-8823');
 
 // Удаляем контакт по id
-removeContact(3);
+// removeContact(3);
